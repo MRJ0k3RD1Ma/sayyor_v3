e85df395cca92cdd543d58f89e47179ddf7ec1c2
Qalandar Dev <64014854+QalandarDev@users.noreply.github.com>
2022-03-10 18:36:31 +0500

    SampleStypesSearch

--- common/models/search/SampleTypesSearch.php
+++ common/models/search/SampleTypesSearch.php
@@ -2,9 +2,17 @@
 
 namespace common\models\search;
 
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Yii;
+use yii\base\Exception;
+use yii\base\InvalidConfigException;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use common\models\SampleTypes;
+use yii\db\QueryInterface;
+use yii\helpers\FileHelper;
 
 /**
  * SampleTypesSearch represents the model behind the search form of `app\models\SampleTypes`.
@@ -67,5 +75,46 @@ class SampleTypesSearch extends SampleTypes
             ->orFilterWhere(['like', 'name_ru', $this->q]);
 
         return $dataProvider;
+    }
+
+    /**
+     * @throws Exception
+     * @throws Exception
+     * @throws InvalidConfigException
+     * @throws \PhpOffice\PhpSpreadsheet\Writer\Exception
+     */
+    public function exportToExcel(?QueryInterface $query)
+    {
+        $speadsheet = new Spreadsheet();
+        $sheet = $speadsheet->getActiveSheet();
+        $title = "Sheet1";
+        $sheet->setTitle(substr($title, 0, 31));
+        $row = 1;
+        $col = 1;
+        $sheet->setCellValueExplicitByColumnAndRow($col++, $row, "#", DataType::TYPE_STRING);
+        $sheet->setCellValueExplicitByColumnAndRow($col++, $row, "Nomi(O'zbek)", DataType::TYPE_STRING);
+        $sheet->setCellValueExplicitByColumnAndRow($col++, $row, "Nomi(Rus)", DataType::TYPE_STRING);
+        $key = 0;
+        $models = $query->all();
+        foreach ($models as $item) {
+            /**
+             * @var SampleTypes $item
+             */
+            $row++;
+            $col = 1;
+            $key++;
+            $sheet->setCellValueExplicitByColumnAndRow($col++, $row, $key, DataType::TYPE_NUMERIC);
+            $sheet->setCellValueExplicitByColumnAndRow($col++, $row, $item->name_uz, DataType::TYPE_STRING);
+            $sheet->setCellValueExplicitByColumnAndRow($col++, $row, $item->name_ru, DataType::TYPE_STRING);
+        }
+        $name = 'ExcelReport.xlsx';
+        $writer = new Xlsx($speadsheet);
+        $dir = Yii::getAlias('@tmp/excel');
+        if (!is_dir($dir)) {
+            FileHelper::createDirectory($dir, 0777);
+        }
+        $fileName = $dir . DIRECTORY_SEPARATOR . $name;
+        $writer->save($fileName);
+        return Yii::$app->response->sendFile($fileName);
     }
 }

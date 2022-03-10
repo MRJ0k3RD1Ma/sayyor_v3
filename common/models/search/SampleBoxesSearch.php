03c1d23e50c846887b94b9dc26e1c33292dbbeaa
Qalandar Dev <64014854+QalandarDev@users.noreply.github.com>
2022-03-10 18:38:14 +0500

    SampleBoxesSearch

--- common/models/search/SampleBoxesSearch.php
+++ common/models/search/SampleBoxesSearch.php
@@ -2,9 +2,16 @@
 
 namespace common\models\search;
 
+use common\models\SampleTypes;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use common\models\SampleBoxes;
+use yii\db\QueryInterface;
+use yii\helpers\FileHelper;
 
 /**
  * SampleBoxesSearch represents the model behind the search form of `app\models\SampleBoxes`.
@@ -67,5 +74,39 @@ class SampleBoxesSearch extends SampleBoxes
             ->orFilterWhere(['like', 'name_ru', $this->q]);
 
         return $dataProvider;
+    }
+    public function exportToExcel(?QueryInterface $query)
+    {
+        $speadsheet = new Spreadsheet();
+        $sheet = $speadsheet->getActiveSheet();
+        $title = "Sheet1";
+        $sheet->setTitle(substr($title, 0, 31));
+        $row = 1;
+        $col = 1;
+        $sheet->setCellValueExplicitByColumnAndRow($col++, $row, "#", DataType::TYPE_STRING);
+        $sheet->setCellValueExplicitByColumnAndRow($col++, $row, "Nomi(O'zbek)", DataType::TYPE_STRING);
+        $sheet->setCellValueExplicitByColumnAndRow($col++, $row, "Nomi(Rus)", DataType::TYPE_STRING);
+        $key = 0;
+        $models = $query->all();
+        foreach ($models as $item) {
+            /**
+             * @var SampleBoxes $item
+             */
+            $row++;
+            $col = 1;
+            $key++;
+            $sheet->setCellValueExplicitByColumnAndRow($col++, $row, $key, DataType::TYPE_NUMERIC);
+            $sheet->setCellValueExplicitByColumnAndRow($col++, $row, $item->name_uz, DataType::TYPE_STRING);
+            $sheet->setCellValueExplicitByColumnAndRow($col++, $row, $item->name_ru, DataType::TYPE_STRING);
+        }
+        $name = 'ExcelReport.xlsx';
+        $writer = new Xlsx($speadsheet);
+        $dir = Yii::getAlias('@tmp/excel');
+        if (!is_dir($dir)) {
+            FileHelper::createDirectory($dir, 0777);
+        }
+        $fileName = $dir . DIRECTORY_SEPARATOR . $name;
+        $writer->save($fileName);
+        return Yii::$app->response->sendFile($fileName);
     }
 }

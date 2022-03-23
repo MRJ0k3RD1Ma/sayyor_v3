bfb2a233dd276aa31b0d9fda96e4ccb2b5a0fb64
Dilmurod Allabergenov <mr.j0k3r.d1ma@gmail.com>
2022-03-23 15:18:01 +0500

    wdf

--- common/models/Soato.php
+++ common/models/Soato.php
@@ -64,8 +64,8 @@ class Soato extends \yii\db\ActiveRecord
     }
     public static function Full($code,$lang='lot'){
         $soato=self::findOne($code);
-        $region = $soato->region_id;
-        $district = $soato->district_id;
+        $region = self::find()->where(['region_id'=>$soato->region_id])->one();
+        $district = self::find()->where(['region_id'=>$soato->region_id])->andWhere(['district_id'=>$soato->district_id])->one();
         return $region->{'name_'.$lang}."<br>".$district->{'name_'.$lang};
     }
 }

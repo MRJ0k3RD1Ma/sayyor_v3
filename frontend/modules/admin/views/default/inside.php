ee1bd30978677377d24fc333803ebbeae101bb0a
QalandarDev <uzwebdevolopersn2@gmail.com>
2022-03-26 12:53:08 +0500

    ReportFoodCategory CRUD+ReportDrugTypes CRUD cp/

--- frontend/modules/admin/views/default/inside.php
+++ frontend/modules/admin/views/default/inside.php
@@ -309,12 +309,49 @@
                 </a>
             </div>
 
+            <div class="col-md-4">
+                <a href="<?= Yii::$app->urlManager->createUrl(['/cp/drug-types']) ?>">
+                    <div class="card">
+                        <!-- card body -->
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-10">
+
+                                    <h5 class="text-muted mb-3 lh-1 d-block text-truncate"><?= Yii::t('cp', 'Report Drug Types') ?></h5>
+                                </div>
+                                <div class="col-2">
+                                    <i class="fa fa-list"></i>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-4">
+                <a href="<?= Yii::$app->urlManager->createUrl(['/cp/report-food']) ?>">
+                    <div class="card">
+                        <!-- card body -->
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-10">
+
+                                    <h5 class="text-muted mb-3 lh-1 d-block text-truncate"><?= Yii::t('cp', 'Report Food Category') ?></h5>
+                                </div>
+                                <div class="col-2">
+                                    <i class="fa fa-list"></i>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </div>
 
 <style>
-    i{
-        font-size:35px;
+    i {
+        font-size: 35px;
     }
 </style>

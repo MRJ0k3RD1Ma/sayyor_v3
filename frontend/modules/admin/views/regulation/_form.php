fc73095732aac7a6867b12caca3d7e22cd218114
QalandarDev <uzwebdevolopersn2@gmail.com>
2022-03-26 12:41:48 +0500

    Regulations +fileinput, saveFile after create,deleteaction and delete if blank

--- frontend/modules/admin/views/regulation/_form.php
+++ frontend/modules/admin/views/regulation/_form.php
@@ -1,7 +1,7 @@
 <?php
 
-use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Regulations */
@@ -16,11 +16,27 @@ use yii\bootstrap4\ActiveForm;
 
     <?= $form->field($model, 'name_ru')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'file')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'file')->widget(\kartik\file\FileInput::class,
+        [
+            'name' => 'file',
+            'pluginOptions' => [
+                'showPreview' => false,
+                'showCaption' => true,
+                'showRemove' => true,
+                'showUpload' => false
+            ]
+        ]) ?>
 
-    <?= $form->field($model, 'creator_id')->hiddenInput(['value'=>Yii::$app->user->identity->getId()])->label(false) ?>
+    <?= $form->field($model, 'creator_id')->hiddenInput(['value' => Yii::$app->user->identity->getId()])->label(false) ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?= $form->field($model, 'status')->widget(\kartik\select2\Select2::class, [
+        'data' => \yii\helpers\ArrayHelper::map(\common\models\StatusList::find()->asArray()->all(), 'id', 'name'),
+        'bsVersion'=>'4.x',
+        'theme' => \kartik\select2\Select2::THEME_CLASSIC,
+        'size' => \kartik\select2\Select2::SIZE_MEDIUM ,
+        'options' => [
+        ]
+    ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>

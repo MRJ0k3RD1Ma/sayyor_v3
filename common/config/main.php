32e17910b63ad808ffb207d41615d41036317824
QalandarDev <uzwebdevolopersn2@gmail.com>
2022-04-02 04:45:10 +0500

    Pager

--- common/config/main.php
+++ common/config/main.php
@@ -4,6 +4,11 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm' => '@vendor/npm-asset',
     ],
+    'container' => [
+        'definitions' => [
+            \yii\widgets\LinkPager::class => \yii\bootstrap4\LinkPager::class,
+        ],
+    ],
     'language' => 'uz',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
@@ -12,9 +17,5 @@ return [
         ],
 
     ],
-    'container' => [
-        'definitions' => [
-            \yii\widgets\LinkPager::class => \yii\bootstrap4\LinkPager::class,
-        ],
-    ],
+    
 ];
